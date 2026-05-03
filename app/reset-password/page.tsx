a7c146bc18dead768fc4d5fa12aa9241bd770676
samiulSozib <samiulcse2018@gmail.com>
2026-05-03 09:40:47 +0600

    update

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -2,16 +2,15 @@
 'use client';
 
 import ResetPasswordForm from '@/components/reset-password/reset-password-form';
-import { Suspense } from 'react';
 
 export default function ResetPasswordPage() {
   return (
-    <Suspense fallback={
+    <div>
       <div className="min-h-screen flex items-center justify-center">
         <div className="animate-spin rounded-full h-8 w-8 border-2 border-[#5F0015] border-t-transparent" />
       </div>
-    }>
+
       <ResetPasswordForm />
-    </Suspense>
+    </div>
   );
 }
